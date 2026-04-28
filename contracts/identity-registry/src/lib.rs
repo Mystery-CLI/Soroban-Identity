f85c5a1b11b96b853f072fb1e37a909fce83f3e4
Marvy <marvy@example.com>
2026-04-28 05:41:55 +0100

    fix: format code with rustfmt

--- contracts/identity-registry/src/lib.rs
+++ contracts/identity-registry/src/lib.rs
@@ -1,8 +1,8 @@
 #![no_std]
 
 use soroban_sdk::{
-    contract, contracterror, contractimpl, contracttype, symbol_short, Address, Bytes, BytesN, Env, Map,
-    String, Symbol,
+    contract, contracterror, contractimpl, contracttype, symbol_short, Address, Bytes, BytesN, Env,
+    Map, String, Symbol,
 };
 
 // ── Errors ────────────────────────────────────────────────────────────────────
